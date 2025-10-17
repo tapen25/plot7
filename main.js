92a2d9d4e68caa4599dc00f27600e46a7effe7a1
tapen25 <ayukky6245@gmail.com>
2025-10-17 16:55:14 +0900

    main.js update

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const statusDiv = document.getElementById('status');
 const cadenceDiv = document.getElementById('cadence');
 
 // 定数（チューニング可能）
-const PEAK_THRESHOLD = 0.8;      // 歩行判定の加速度閾値
-const STEP_INTERVAL_MS = 300;    // チャタリング防止
-const HISTORY_SECONDS = 5;       // ケイデンス算出に使う過去時間
+const PEAK_THRESHOLD = 1.2;      // 「1歩」として検知する閾値
+const STEP_INTERVAL_MS = 300;    // チャタリング防止　1回の歩行で複数回カウントすることを防ぐ
+const HISTORY_SECONDS = 5;       // ケイデンス算出に使う過去時間　直近何秒間の歩数データを使うか
 const STILL_THRESHOLD = 30;      // 静止とみなす閾値
 const WALK_THRESHOLD = 110;      // 歩行/速歩の閾値
 
